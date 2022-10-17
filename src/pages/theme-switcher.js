dcef499c6083f28a181aa2e2ed8af6b2e8807c40
Karun S <karoon.siri@gmail.com>
2022-10-17 23:17:27 +0700

    Darkmode improved
    Use 'class' strategy for dark mode to manually toggle between modes

---
+++ src/pages/theme-switcher.js
@@ -0,0 +1,47 @@
+import React, { useContext, useEffect } from "react"
+import { GlobalContext } from "../components/GlobalContextProvider"
+
+const DarkModePage = () => {
+  const { mode, setMode } = useContext(GlobalContext)
+
+  useEffect(() => {
+    if (mode === 'dark') {
+      document.documentElement.classList.add('dark')
+    }
+    else {
+      document.documentElement.classList.remove('dark')
+    }
+  }, [mode])
+
+  return (
+    <div className="grid h-screen place-items-center">
+      <div
+        className={`
+          container mx-auto rounded w-1/2 p-8
+          bg-base shadow-lg shadow-base text-base
+          ${mode === 'dark' && 'theme-dark'}
+        `}
+      >
+        <h1 className="text-3xl font-semibold mb-4">
+          Hello Tailwind
+        </h1>
+
+        <p className="p-4 border border-base rounded-lg mb-4">
+          This is the test text. Interesting, isn't it?
+        </p>
+
+        <button
+          className={`
+            p-4 rounded bg-slate-600 text-slate-50 transition ease-in-out duration-150
+            hover:bg-slate-300 hover:text-slate-700
+          `}
+          onClick={() => setMode(mode === 'light' ? 'dark' : 'light')}
+        >
+          Toggle Dark Mode
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default DarkModePage
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
+  darkMode: 'class',
   content: [
     "./gatsby-ssr.js",
     "./src/pages/**/*.{js,jsx,ts,tsx}",
     "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        base: 'var(--color-bg)',
+      },
+      borderColor: {
+        base: 'var(--color-border)',
+      },
+      boxShadowColor: {
+        base: 'var(--color-shadow)',
+      },
+      textColor: {
+        base: 'var(--color-text-base)',
+        muted: 'var(--color-text-muted)',
+      },
+    },
   },
   plugins: [],
 }
